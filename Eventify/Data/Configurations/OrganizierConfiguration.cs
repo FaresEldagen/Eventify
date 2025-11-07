1aaf31f6eb02bb764ccfc6225a7e0b4f2c00d1b8
Zyad <zyadpash8@gmail.com>
2025-11-07 13:33:39 +0200

    Added Organizier Model and Own Configuration

---
+++ Eventify/Data/Configurations/OrganizierConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using Eventify.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eventify.Data.Configurations
+{
+    public class OrganizierConfiguration : IEntityTypeConfiguration<Organizier> 
+    {
+        public void Configure(EntityTypeBuilder<Organizier> builder)
+        {
+            builder.ToTable("Organiziers");
+
+            builder.HasKey(e => e.OrganizierId);
+
+            builder.Property(e => e.Password)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255).IsRequired();
+
+            builder.Property(e => e.Photo)
+                .HasColumnType("VARCHAR").HasMaxLength(150);
+
+            builder.Property(e => e.Username)
+                .HasColumnType("VARCHAR").HasMaxLength(150).IsRequired();
+
+            builder.Property(e => e.Gender)
+                .HasConversion<int>().IsRequired();
+
+            builder.Property(e => e.BIO)
+                .HasColumnType("VARCHAR(MAX)");
+
+            builder.Property(e => e.Country)
+                .HasColumnType("VARCHAR").HasMaxLength(50).IsRequired();
+
+            builder.Property(e => e.ExperienceYear)
+                .HasColumnType("INT").HasMaxLength(50);
+
+            builder.Property(e => e.PastEventCount)
+                .HasColumnType("INT").HasMaxLength(50);
+
+            builder.Property(e => e.Specialization)
+                .HasColumnType("VARCHAR").HasMaxLength(50);
+
+            builder.Property(e => e.Email)
+                .HasColumnType("VARCHAR").HasMaxLength(50).IsRequired();
+
+            builder.Property(e => e.FrontIDPhoto)
+                .HasColumnType("VARCHAR").HasMaxLength(255).IsRequired();
+
+            builder.Property(e => e.BackIDPhoto)
+                .HasColumnType("VARCHAR").HasMaxLength(255).IsRequired();
+
+            builder.Property(e => e.NationalIDNumber)
+                .HasColumnType("VARCHAR").HasMaxLength(255).IsRequired();
+
+            builder.Property(e => e.ArabicAddress)
+                .HasColumnType("VARCHAR").HasMaxLength(255);
+
+            builder.Property(e => e.ArabicFullName)
+                .HasColumnType("VARCHAR").HasMaxLength(100);
+
+            builder.Property(e => e.CellPhone)
+                .HasColumnType("VARCHAR").HasMaxLength(11).IsRequired();
+
+            builder.Property(e => e.JoinedDate)
+                .HasColumnType("DATETIME2").IsRequired();
+        }
+
+    }
+
+}
---
+++ Eventify/Models/Entities/Organizier.cs
@@ -0,0 +1,28 @@
+﻿using Eventify.Models.Enums;
+
+namespace Eventify.Models.Entities
+{
+    public class Organizier
+    {
+        public int OrganizierId { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Photo { get; set; }
+        public GenderEnum Gender { get; set; }
+        public string? BIO { get; set; }
+        public string? Country { get; set; }
+        public int ExperienceYear { get; set; }
+        public int PastEventCount { get; set; }
+        public string? Specialization { get; set; }
+        public string? Email { get; set; }
+        public string? FrontIDPhoto { get; set; }
+        public string? BackIDPhoto { get; set; }
+        public string? NationalIDNumber { get; set; }
+        public string? ArabicAddress { get; set; }
+        public string? ArabicFullName { get; set; }
+        public string? CellPhone { get; set; }
+        public DateTime JoinedDate { get; set; }
+
+
+    }
+}
