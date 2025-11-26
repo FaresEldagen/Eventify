b3dd9b03830b145d8aed64cee2382cd4a5b82cde
Fares <fareseldagen1@gmail.com>
2025-11-26 21:52:20 +0200

    Add Frontend Prototype Pages

---
+++ Eventify/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult OtherVenueOwner()
+        {
+            return View();
+        }
+        public IActionResult OtherEventOrganizer()
+        {
+            return View();
+        }
+        public IActionResult OtherAttendee()
+        {
+            return View();
+        }
+        public IActionResult PersonalVenueOwner()
+        {
+            return View();
+        }
+        public IActionResult PersonalEventOrganizer()
+        {
+            return View();
+        }
+        public IActionResult PersonalEventOrganizer2()
+        {
+            return View();
+        }
+        public IActionResult PersonalAttendee()
+        {
+            return View();
+        }
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+    }
+}
