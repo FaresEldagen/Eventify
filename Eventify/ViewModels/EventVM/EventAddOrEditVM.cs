15d6f0a0f2689f0dfb60c6b9a019abca04e7e82d
AmrElsaed <itz.vmr@gmail.com>
2025-11-28 18:37:28 +0200

    Event VM And Controller

---
+++ Eventify/ViewModels/EventVM/EventAddOrEditVM.cs
@@ -0,0 +1,19 @@
+﻿using Eventify.Models.Entities;
+using Eventify.Models.Enums;
+
+namespace Eventify.ViewModels.EventVM
+{
+    public class EventAddOrEditVM
+    {
+        public string? EventTitle { get; set; }
+        public EventCategoryEnum Category { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public decimal TicketPrice { get; set; }
+        public string? Features { get; set; }
+        public bool IsPrivate { get; set; } = false;
+        public List<EventPhoto> EventPhotos { get; set; } = new List<EventPhoto>();
+
+    }
+}
