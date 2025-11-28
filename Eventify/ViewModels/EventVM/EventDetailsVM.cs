bfe1d5b315fdcfa453127e139c1cac88d3e94a42
AmrElsaed <itz.vmr@gmail.com>
2025-11-28 19:33:39 +0200

    Fix

--- Eventify/ViewModels/EventVM/EventDetailsVM.cs
+++ Eventify/ViewModels/EventVM/EventDetailsVM.cs
@@ -1,6 +1,22 @@
-﻿namespace Eventify.ViewModels.EventVM
+﻿using Eventify.Models.Entities;
+using Eventify.Models.Enums;
+
+namespace Eventify.ViewModels.EventVM
 {
     public class EventDetailsVM
     {
+        public string? EventTitle { get; set; }
+        public string? Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public EventStatusEnum Status { get; set; } = EventStatusEnum.Pending;
+        public decimal TicketPrice { get; set; }
+        public string? Features { get; set; }
+        public string? Address { get; set; }
+        public string OrganizerName { get; set; }
+        public int OrganizerId { get; set; }
+        public int Capacity { get; set; }
+        public List<EventPhoto> EventPhotos { get; set; } = new List<EventPhoto>();
+        public EventCategoryEnum Category { get; set; }
+
     }
 }
