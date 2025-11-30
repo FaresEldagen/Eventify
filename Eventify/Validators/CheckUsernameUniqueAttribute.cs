5f3e00394e5751b486c24fc57db8d6e7035a1f53
Ahmed Albatrawy <albatrawyahmed@gmail.com>
2025-11-30 23:00:13 +0200

    Add-CheckUsername: - Create "CheckUsernameUniqueAttribute.cs" - Use CheckUsernameUnique Validator in "SignupViewModel.cs"

---
+++ Eventify/Validators/CheckUsernameUniqueAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Eventify.Data;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eventify.Validators
+{
+    public class CheckUsernameUniqueAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var context = validationContext.GetService<AppDbContext>();
+                var isExist = context!.Users.Any(u => u.UserName == value.ToString());
+                if (isExist)
+                {
+                    return new ValidationResult("This Username already Exists");
+                }
+                return ValidationResult.Success;
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
