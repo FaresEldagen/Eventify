b20bf96098632380673903e5e366ff961c41e461
Fares <fareseldagen1@gmail.com>
2025-12-06 13:14:36 +0200

    Adding Fontend Pages

---
+++ Eventify/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eventify.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddAdmin()
+        {
+            return View();
+        }
+        public IActionResult ReviewUser()
+        {
+            return View();
+        }
+        public IActionResult VerifyUser()
+        {
+            return View();
+        }
+        public IActionResult DeclineUser()
+        {
+            return View();
+        }
+        public IActionResult ReviewVenue()
+        {
+            return View();
+        }
+        public IActionResult VerifyVenue()
+        {
+            return View();
+        }
+        public IActionResult DeclineVenue()
+        {
+            return View();
+        }
+        public IActionResult ReviewEvent()
+        {
+            return View();
+        }
+        public IActionResult VerifyEvent()
+        {
+            return View();
+        }
+        public IActionResult DeclineEvent()
+        {
+            return View();
+        }
+    }
+}
---
+++ Eventify/ViewModels/AdminVm/AdminDashboardVM.cs
@@ -0,0 +1,13 @@
+﻿using Eventify.Models.Entities;
+
+namespace Eventify.ViewModels.AdminVm
+{
+    public class AdminDashboardVM
+    {
+        public List<Admin> Admins = new List<Admin>();
+        public List<ApplicationUser> Users = new List<ApplicationUser>();
+        public List<Venue> Venues = new List<Venue>();
+        public List<Event> Events = new List<Event>();
+        public List<Payment> Payments = new List<Payment>();
+    }
+}
